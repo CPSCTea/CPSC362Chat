7a398da0db797186050c5a8d4a9ce9ce653d2337
CPSCTea <aznhazmat@gmail.com>
2016-09-28 10:05:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
