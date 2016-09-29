d5031ea634125271db41bffcb5375a51f73164a1
CPSCTea <aznhazmat@gmail.com>
2016-09-29 01:47:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(port){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
