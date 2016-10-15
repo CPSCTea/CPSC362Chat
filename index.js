3d77d20c2ab8d666a02230566642c3faa112ecc4
PayneGray <rlacsamana@csu.fullerton.edu>
2016-10-15 09:55:46 -0700

    more fixes

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = 8080//process.env.PORT
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html');
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
   socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:Port:');
-  console.log(port);
+http.listen(8080, '0.0.0.0',function(){
+  console.log('listening on *:8080');
 });
+
