ef6f7f2411a454200fe3f636ba9d0a5cef307d61
CPSCTea <aznhazmat@gmail.com>
2016-10-26 22:58:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function (socket) {
+  socket.broadcast.emit('User connected');
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
+
+io.on('connection', function(socket){
+  socket.on('user name', function(user){
+    io.emit('user name', user);
+  });
+});
+
+socket.on('disconnect', function () {
+    io.emit('User disconnected');
+ });
 
 http.listen(port, function(){
   console.log('listening on *:Port:');
