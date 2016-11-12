8b4229786d8ffda44688f44b40c1b6e106f2f01e
CPSCTea <aznhazmat@gmail.com>
2016-11-12 09:31:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT;
+var list[20];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -10,6 +11,29 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', function(log){
+  log.on('append', function(msg){
+    var mod = false;
+    for (int i = 0; i<20; i++)
+    {
+      if (list[i]=='')
+      {
+        list[i]=msg;
+        mod= true;
+      }
+    }
+    if (mod == false)
+    {
+      for (int i = 0; i<19; i++)
+      {
+        list[i] = list[i+1];
+      }
+      list[19]=msg;
+    }
+    msg = list;
   });
 });
 
