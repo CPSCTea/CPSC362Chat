daaa28c01220f6677547debf38305db2a591abeb
PayneGray <rlacsamana@csu.fullerton.edu>
2016-11-12 00:50:38 -0800

    debugginh

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-
+});
 http.listen(port, function(){
   console.log('listening on *:Port:');
   console.log(port);
