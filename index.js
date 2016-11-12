415b40463bc724bae3d99adf1eaa9b7ffa43b0ab
PayneGray <rlacsamana@csu.fullerton.edu>
2016-11-12 00:48:34 -0800

    cleaning

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT
-
+var name = "Payne";
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,7 +12,6 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
 
 http.listen(port, function(){
   console.log('listening on *:Port:');
